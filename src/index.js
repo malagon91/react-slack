8ab339227d1b3421b204358346d0d688d709a7a0
Miguel <miguelmalagon@outlook.com>
2019-04-17 23:35:48 -0500

    add new components

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
 import registerServiceWorker from './registerServiceWorker';
 
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
@@ -10,6 +12,8 @@ const Root = () => (
   <Router>
     <Switch>
       <Route path="/" component={App} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
     </Switch>
   </Router>);
 
