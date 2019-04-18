6574886ed06cf6df6ba6777dad604ee2cfa96c44
Miguel <miguelmalagon@outlook.com>
2019-04-18 13:58:15 -0500

    format

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,13 +1,49 @@
 import React, {Component} from 'react';
 import {Grid, Form, Segment, Button, Header, Message, Icon} from 'semantic-ui-react';
+import {Link} from 'react-router-dom';
 
+class Register extends Component {
+  state = {
+    username: '',
+    email: '',
+    password: '',
+    passwordConfirmation: '',
+  };
 
-class Register extends Component{
-  render(){
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  };
+
+  render() {
+    const {username, email, password, passwordConfirmation} = this.state;
+
     return (
-      <Grid textAlign="center" verticalAlign="middle">
+      <Grid textAlign="center" verticalAlign="middle" className="app">
         <Grid.Column style={{maxWidth: 450}}>
+          <Header as="h2" icon color="orange" textAlign="center">
+            <Icon name="puzzle piece" color="orange"/>
+            Register for DevChat
+          </Header>
+          <Form size="large">
+            <Segment stacked>
+              <Form.Input fluid name="username" icon="user" iconPosition="left" value={username}
+                          placeholder="UserName" onChange={this.handleChange} type="text"/>
 
+              <Form.Input fluid name="email" icon="mail" iconPosition="left" value={email}
+                          placeholder="email address" onChange={this.handleChange} type="email"/>
+
+              <Form.Input fluid name="password" icon="lock" iconPosition="left" value={password}
+                          placeholder="Password" onChange={this.handleChange} type="password"/>
+
+              <Form.Input fluid name="passwordConfirmation" icon="repeat" iconPosition="left" value={passwordConfirmation}
+                          placeholder="Password Confirmation" onChange={this.handleChange} type="password"/>
+
+              <Button color="orange" fluid size="large">Submit</Button>
+            </Segment>
+          </Form>
+          <Message>Already a user? <Link to="/login">Login</Link></Message>
         </Grid.Column>
       </Grid>
     );
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 const Root = () => (
   <Router>
     <Switch>
-      <Route path="/" component={App} />
+      <Route exact path="/" component={App} />
       <Route path="/login" component={Login} />
       <Route path="/register" component={Register} />
     </Switch>
